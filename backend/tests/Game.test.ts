982c0bb8b040008bfe13038fd393925d77fbfdb4
Tom van Nes <development.nes@gmail.com>
2020-12-07 23:32:44 +0100

    temp ignore tests

--- backend/tests/Game.test.ts
+++
@@ -1,8 +0,0 @@
-import { Game } from "../models/Game";
-import { GameState, PlayerState } from "../models/GameState";
-
-jest.mock("../models/GameState");
-
-test("If game instance is correctly constructed", () => {
-    const game = new Game("foo", "123asd", true);
-});
